4d597a07765e37636232db1f97406e612fba027d
Alex Jercan <jercan_alex27@yahoo.com>
2025-10-27 08:46:04 +0200

    fix: debug camera should still show the egui context

--- crates/nova_debug/src/lib.rs
+++ crates/nova_debug/src/lib.rs
@@ -2,10 +2,6 @@ use bevy::{
     pbr::wireframe::{WireframeConfig, WireframePlugin},
     prelude::*,
 };
-use bevy_inspector_egui::{
-    bevy_egui::{EguiContext, EguiPlugin, EguiPrimaryContextPass, PrimaryEguiContext},
-    bevy_inspector, egui, DefaultInspectorConfigPlugin,
-};
 
 pub mod prelude {
     pub use super::gizmos::DebugAxisMarker;
@@ -53,6 +49,12 @@ fn update_physics_gizmos(debug: Res<DebugEnabled>, mut store: ResMut<GizmoConfig
 mod inspector {
     use super::*;
 
+    use bevy_inspector_egui::{
+        bevy_egui,
+        bevy_egui::{EguiContext, EguiPlugin, EguiPrimaryContextPass, PrimaryEguiContext},
+        egui, DefaultInspectorConfigPlugin,
+    };
+
     /// A plugin that adds an inspector UI for debugging.
     pub struct InpsectorDebugPlugin;
 
@@ -66,21 +68,56 @@ mod inspector {
                     EguiPrimaryContextPass,
                     inspector_ui.run_if(resource_equals(DebugEnabled(true))),
                 );
+
+            app.insert_resource(bevy_egui::EguiGlobalSettings {
+                auto_create_primary_context: false,
+                ..Default::default()
+            });
+
+            // TODO: Ideally we would have an extra camera for the inspector only, but for now we
+            // will just use the primary camera.
+            app.add_observer(on_add_camera);
         }
     }
 
     fn inspector_ui(world: &mut World) {
-        let mut egui_context = world
+        let Ok(egui_context) = world
             .query_filtered::<&mut EguiContext, With<PrimaryEguiContext>>()
             .single(world)
-            .expect("EguiContext not found")
-            .clone();
+        else {
+            warn!("inspector_ui: No EguiContext found");
+            return;
+        };
+        let mut egui_context = egui_context.clone();
 
         egui::Window::new("UI").show(egui_context.get_mut(), |ui| {
-            egui::ScrollArea::both().show(ui, |ui| {
-                bevy_inspector::ui_for_world(world, ui);
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                // equivalent to `WorldInspectorPlugin`
+                bevy_inspector_egui::bevy_inspector::ui_for_world(world, ui);
+
+                egui::CollapsingHeader::new("Materials").show(ui, |ui| {
+                    bevy_inspector_egui::bevy_inspector::ui_for_assets::<StandardMaterial>(
+                        world, ui,
+                    );
+                });
+
+                ui.heading("Entities");
+                bevy_inspector_egui::bevy_inspector::ui_for_entities(world, ui);
             });
         });
+    }
+
+    fn on_add_camera(
+        add: On<Add, Camera>,
+        mut commands: Commands,
+    ) {
+        let entity = add.entity;
+        debug!(
+            "Camera added to entity {:?}, inserting PrimaryEguiContext to it.",
+            entity
+        );
+
+        commands.entity(entity).insert(PrimaryEguiContext);
     }
 }
 
